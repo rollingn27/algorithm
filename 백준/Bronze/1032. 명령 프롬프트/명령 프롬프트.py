2928e6fdc55c86ab934317f39d40335651f3ea51
rollingn27 <106942510+rollingn27@users.noreply.github.com>
2023-01-09 11:00:44 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -6,7 +6,7 @@ for _ in range(n):
 
 file_len = len(data[0])
 
-answer = result = data[0]
+result = data[0]
 
 result_list = list(result)
 
@@ -14,5 +14,5 @@ for i in range(file_len):
   for j in range(1, len(data)):
     if(data[j - 1][i] != data[j][i]):
       result_list[i] = '?'
-      answer = "".join(result_list)
-print(answer)
+      result = "".join(result_list)
+print(result)
