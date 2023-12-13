c95d8393331db3922d1a8df0fc1f35245e113a07
rollingn27 <106942510+rollingn27@users.noreply.github.com>
2023-12-13 22:51:21 +0900

    [Silver IV] Title: 카드, Time: 292 ms, Memory: 29528 KB -BaekjoonHub

---
+++ 백준/Silver/1835. 카드/카드.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Deque<Integer> dq = new LinkedList<>();
+        Deque<Integer> makeDq = new LinkedList<>();
+        dq.add(N);
+        for (int i = N - 1; i > 0; i--) {
+
+            dq.add(i);
+            for (int j = 0; j < i; j++) {
+
+                dq.add(dq.pop());
+            }
+        }
+
+        while (!dq.isEmpty()) {
+            System.out.println(dq.pollLast());
+        }
+    }
+}
