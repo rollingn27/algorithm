7389617af87cd75c5bca14595dae465715e362a4
rollingn27 <106942510+rollingn27@users.noreply.github.com>
2024-01-01 10:26:13 +0900

    [Silver V] Title: 학생 인기도 측정, Time: 184 ms, Memory: 17784 KB -BaekjoonHub

--- 백준/Silver/25325. 학생 인기도 측정/학생 인기도 측정.java
+++ 백준/Silver/25325. 학생 인기도 측정/학생 인기도 측정.java
@@ -1,40 +1,32 @@
+import java.io.*;
 import java.util.*;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        String A = sc.nextLine();
-        String[] names = A.split(" ");
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            int n = Integer.parseInt(br.readLine());
+            String[] students = br.readLine().split(" ");
+            Map<String, Integer> studentMap = new HashMap<>();
+            for (int i = 0; i < n; i++) {
+                studentMap.put(students[i], 0);
+            }
+            for (int i = 0; i < n; i++) {
+                String[] likes = br.readLine().split(" ");
+                for (String like: likes) {
+                    studentMap.put(like, studentMap.getOrDefault(like, 0) + 1);
+                }
+            }
+            PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((a, b) -> {
+                int compare = b.getValue() - a.getValue();
 
-        Map<String, Integer> namesMap = new HashMap<>();
-        for (String name: names) {
-            namesMap.put(name, 0);
-        }
-
-        for (int i = 0; i < n; i++) {
-            String love = sc.nextLine();
-            String[] loves = love.split(" ");
-            for(String l: loves) {
-                namesMap.put(l, namesMap.get(l) + 1);
+                return compare == 0 ? a.getKey().compareTo(b.getKey()) : compare;
+            });
+            pq.addAll(studentMap.entrySet());
+            while (!pq.isEmpty()) {
+                Map.Entry<String, Integer> print = pq.poll();
+                bw.write(print.getKey() + " " + print.getValue() + "\n");
             }
         }
-
-        Comparator<Map.Entry<String, Integer>> customComparator = (entry1, entry2) -> {
-            int valueCompare = entry2.getValue().compareTo(entry1.getValue());
-
-            return (valueCompare == 0) ? entry1.getKey().compareTo(entry2.getKey()) : valueCompare;
-        };
-
-        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(customComparator);
-        pq.addAll(namesMap.entrySet());
-
-        while (!pq.isEmpty()) {
-            Map.Entry<String, Integer> entry = pq.poll();
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
-
-    }    
+    }
 }
-
