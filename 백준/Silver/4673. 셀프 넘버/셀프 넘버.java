6a3d0027dd65432214b8248cfc6a2ba3dca6d607
rollingn27 <106942510+rollingn27@users.noreply.github.com>
2024-01-01 11:21:48 +0900

    [Silver V] Title: 셀프 넘버, Time: 216 ms, Memory: 15804 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -8,29 +8,21 @@ public class Main {
             List<Integer> numsList = new ArrayList<>();
             for (int i = 1; i < 10000; i++) {
                 numsList.add(i);
-            }
-
-            List<Integer> sumList = new ArrayList<>();
+            }            
             for (int i = 1; i < 10000; i++) {
                 int count = 0;
                 int num = i;
-                String str = String.valueOf(num);
-                
+                String str = String.valueOf(num);                
                 int sum = i;
-
                 for (int j = 0; j < str.length(); j++) {
                     sum += (str.charAt(j) - '0');
-                }
-
-                sumList.add(sum);
-            }
-
-            numsList.removeAll(sumList);
+                }                
+                numsList.remove(Integer.valueOf(sum));                
+            }            
+            
             for (int i : numsList) {
-                bw.write(Integer.toString(i) + "\n");
-            }
+                bw.write(Integer.toString(i) + "\n");                                          
+            }            
         }        
-
     }
-
 }
