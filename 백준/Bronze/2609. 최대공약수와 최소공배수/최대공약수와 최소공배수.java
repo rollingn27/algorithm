a9d42ab27bc6c45f7787b04456a6dd424548d0fd
rollingn27 <106942510+rollingn27@users.noreply.github.com>
2024-01-02 23:11:50 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 144 ms, Memory: 15896 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,29 +1,23 @@
-import java.util.*;
-
+import java.io.*;
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-
-        int gcdNum;
-
-        if (num1 > num2) {
-            gcdNum = gcd(num1, num2);
-        } else {
-            gcdNum = gcd(num2, num1);
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            String[] str = br.readLine().split(" ");
+            int num1 = Integer.parseInt(str[0]);
+            int num2 = Integer.parseInt(str[1]);
+            int gcdNum = gcd(num1, num2);
+            StringBuilder sb = new StringBuilder();
+            sb.append(gcdNum + "\n");
+            sb.append(num1 * num2 / gcdNum);
+            bw.write(sb.toString());
         }
-
-        System.out.println(gcdNum);
-        System.out.println(num1 * num2 / gcdNum);
-
     }
 
     public static int gcd(int num1, int num2) {
         if (num1 % num2 == 0) {
             return num2;
         }
-
         return gcd(num2, num1 % num2);
     }
 }
