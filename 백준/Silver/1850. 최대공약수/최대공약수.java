a06111443754deb87508eef18e6da0c9a18522bd
rollingn27 <106942510+rollingn27@users.noreply.github.com>
2024-01-07 19:54:08 +0900

    [Silver I] Title: 최대공약수, Time: 816 ms, Memory: 39004 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            StringBuilder sb = new StringBuilder();
+            String[] AB = br.readLine().split(" ");
+            long A = Long.parseLong(AB[0]);
+            long B = Long.parseLong(AB[1]);
+            for (long i = 0; i < gcd(A, B); i++) {
+                sb.append(1);
+            }
+            bw.write(sb.toString());
+        }
+    }
+    public static long gcd(long a, long b) {
+        if (a % b == 0)
+            return b;
+        return gcd(b, a % b);
+    }
+}
