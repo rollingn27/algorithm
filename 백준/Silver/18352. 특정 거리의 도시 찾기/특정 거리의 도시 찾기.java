31be87b7a08c76bf6895506847b0b048134418d4
rollingn27 <106942510+rollingn27@users.noreply.github.com>
2024-01-08 21:06:20 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1580 ms, Memory: 337988 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -2,60 +2,56 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
             String[] NMKX = br.readLine().split(" ");
             int N = Integer.parseInt(NMKX[0]);
             int M = Integer.parseInt(NMKX[1]);
             int K = Integer.parseInt(NMKX[2]);
             int X = Integer.parseInt(NMKX[3]);
-            int[] distances = new int[N];
-            Arrays.fill(distances, 1000000);
-            Map<Integer, List<Integer>> graph = new HashMap<>();
+            List<List<Integer>> graph = new ArrayList<>();
+            for (int i = 0; i <= N; i++) {
+                graph.add(new ArrayList<>());
+            }
             for (int i = 0; i < M; i++) {
                 String[] AB = br.readLine().split(" ");
-                graph.putIfAbsent(Integer.parseInt(AB[0]), new LinkedList<>());
-                graph.get(Integer.parseInt(AB[0])).add(Integer.parseInt(AB[1]));
+                int A = Integer.parseInt(AB[0]);
+                int B = Integer.parseInt(AB[1]);
+                graph.get(A).add(B);
             }
-
-            PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-            pq.add(new int[] {0, X});
-            distances[X - 1] = 0;
-            List<Integer> result = new LinkedList<>();
-            while(!pq.isEmpty()) {
-                int[] info = pq.poll();
-                if (distances[info[1] - 1] < info[0]) {
-                    continue;
-                }
-
-                if (graph.get(info[1]) != null) {
-                    for (int i: graph.get(info[1])) {
-                        if (info[0] < distances[i - 1]) {
-                            distances[i - 1] = info[0] + 1;
-                            pq.add(new int[] { distances[i - 1], i});
-                        }
+            int[] distances = new int[N + 1];
+            int infi = Integer.MAX_VALUE;
+            Arrays.fill(distances, infi);
+            Queue<Integer> que = new LinkedList<>();
+            que.add(X);
+            distances[X] = 0;
+            while (!que.isEmpty()) {
+                Integer start = que.poll();
+                int size = graph.get(start).size();
+                for (int i = 0; i < size; i++) {
+                    if (distances[graph.get(start).get(i)] != infi)
+                        continue;
+                    if (distances[graph.get(start).get(i)] == infi) {
+                        distances[graph.get(start).get(i)] = distances[start] + 1;
+                        que.add(graph.get(start).get(i));
                     }
                 }
             }
-            int count = 0;
-            for (int i = 0; i < N; i++) {
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            for (int i = 1; i < distances.length; i++) {
                 if (distances[i] == K) {
-                    count++;
+                    pq.add(i);
                 }
             }
-
-            if (count == 0) {
+            StringBuilder sb = new StringBuilder();
+            if (pq.size() == 0)
                 bw.write("-1");
-            } else {
-                for (int i = 0; i < N; i++) {
-                    if (distances[i] == K) {
-                        bw.write(Integer.toString(i + 1) + "\n");
-                    }
-                }
+            else {
+                while (!pq.isEmpty())
+                    sb.append(pq.poll() + "\n");
             }
+            bw.write(sb.toString());
         }
     }
-
 }
