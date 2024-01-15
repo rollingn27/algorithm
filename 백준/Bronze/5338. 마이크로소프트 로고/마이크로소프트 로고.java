a53dc495132715672657c17e99c8cdd74233a146
rollingn27 <106942510+rollingn27@users.noreply.github.com>
2024-01-15 23:12:49 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 112 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,12 @@
+class Main {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("       _.-;;-._\n");
+        sb.append("'-..-'|   ||   |\n");
+        sb.append("'-..-'|_.-;;-._|\n");
+        sb.append("'-..-'|   ||   |\n");
+        sb.append("'-..-'|_.-''-._|\n");
+        System.out.println(sb);
+    }
+}
