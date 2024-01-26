f3578c07cac5ec807fc817e6ad2524c5913b9ea5
rollingn27 <106942510+rollingn27@users.noreply.github.com>
2024-01-26 23:12:11 +0900

    [Bronze V] Title: 연세대학교, Time: 192 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        String N = sc.next();
+        if (N.equals("0")) {
+            System.out.println("YONSEI");
+        } else {
+            System.out.println("Leading the Way to the Future");
+        }
+
+    }
+}
