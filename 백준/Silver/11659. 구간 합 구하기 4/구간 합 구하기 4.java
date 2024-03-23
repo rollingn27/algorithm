4335d81ba19f1aaffe9e1cf13f136824f47ee799
rollingn27 <106942510+rollingn27@users.noreply.github.com>
2024-03-23 16:53:25 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 696 ms, Memory: 66060 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -3,27 +3,25 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
             String[] NM = br.readLine().split(" ");
-            int n = Integer.parseInt(NM[0]);
-            int m = Integer.parseInt(NM[1]);
-            int[] nums = new int[n];
-            int[] sums = new int[n + 1];
+            int N = Integer.parseInt(NM[0]);
+            int M = Integer.parseInt(NM[1]);
+            long[] S = new long[N + 1];
             String[] Ns = br.readLine().split(" ");
-            for (int i = 0; i < n; i++) {
-                nums[i] = Integer.parseInt(Ns[i]);
+            for (int i = 1; i <= N; i++) {
+                S[i] = S[i - 1] + Long.parseLong(Ns[i - 1]);
             }
-            sums[0] = 0;
-            sums[1] = nums[0];
-            for (int i = 1; i < n; i++) {
-                sums[i + 1] = nums[i] + sums[i];
-            }
-            for (int i = 0; i < m; i++) {
+
+            for (int i = 0; i < M; i++) {
                 String[] IJ = br.readLine().split(" ");
                 int I = Integer.parseInt(IJ[0]);
                 int J = Integer.parseInt(IJ[1]);
-                bw.write(String.valueOf(sums[J] - sums[I - 1]) + "\n");
+                bw.write(Long.toString(S[J] - S[I - 1]) + "\n");
             }
+
         }
     }
+
 }
